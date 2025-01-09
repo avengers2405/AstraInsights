1f273710a30f6dbab26441474ff71b99244a1902
Rushabh Bhalgat <65655146+RushabhBhalgat@users.noreply.github.com>
2025-01-09 21:36:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -549,7 +549,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 
 // Initialize server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at vercel:)`);
   // Connect to Astra DB when server starts
   run().catch(console.error);
 });
